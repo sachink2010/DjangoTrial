11ce2a097c363809e6120b14774810aabfc79a51
Sachin Kulkarni <sachin.kulkarni.iimc@gmail.com>
2021-09-28 23:51:39 +0200

    undoing changes

--- storefront/urls.py
+++ storefront/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
-import debug_toolbar
+#import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
