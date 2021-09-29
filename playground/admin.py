fc479d7c522c1485977aa0977a10f7f3f55cd1ef
Sachin Kulkarni <sachin.kulkarni.iimc@gmail.com>
2021-09-29 23:51:22 +0200

    Added views , template

--- playground/admin.py
+++ playground/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Offer
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('name','price','stock')
+    
+class OfferAdmin(admin.ModelAdmin):
+    list_display=('code','description')
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(Offer,OfferAdmin)
+
+
+
--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 
 def say_hello(request):
     #x=1
     #y=2
-    return render(request,'hello.html',{'name':'Sachin'})
+    products=Product.objects.all()
+    return render(request,'hello.html',{'name':'Sachin','products':products})
     #return HttpResponse('Hello World')
 
