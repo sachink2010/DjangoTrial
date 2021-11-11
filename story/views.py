5ecdbf800c22d0f66d6f3fef4d1af2c820b133e4
Sachin Kulkarni <sachin.kulkarni.iimc@gmail.com>
2021-11-11 22:47:49 +0100

    code display changes

--- story/views.py
+++ story/views.py
@@ -35,7 +35,8 @@ def story(request):
             print(f'username is,', {username})
 
 
-            jurassic_key=os.environ['JURASSIC_KEY']
+            #jurassic_key=os.environ['JURASSIC_KEY']
+            jurassic_key="Bearer eNZ1QKmraxGN9f41ESRL6MZ9zLxzCOwE"
 
             #print(jurassic_key)
             
@@ -94,11 +95,12 @@ def code(request):
            
             openai.api_key = os.environ['OPENAI_KEY']
 
+
             response = openai.Completion.create(
                 engine="davinci-codex",
                 prompt=user_input,
                 temperature=0,
-                max_tokens=100,
+                max_tokens=200,
                 top_p=1.0,
                 frequency_penalty=0.0,
                 presence_penalty=0.0,
